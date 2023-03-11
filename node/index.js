8a59d118c9ce45f6bda6a102994d52a3cb89581c
louisMascarenhas <luis.mascarenhas@kabum.com.br>
2023-03-10 22:23:38 -0300

    refactor: corrigido o texto com base no desafio

--- node/index.js
+++ node/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('<h1> Hello World! </h1>')
+    res.send('<h1> Full Cycle Rocks!</h1>')
 })
 
 app.listen(port, () => {
