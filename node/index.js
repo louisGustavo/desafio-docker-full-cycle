ddca33e9383e837515f9beab4042325f32632106
louisMascarenhas <luis.mascarenhas@kabum.com.br>
2023-03-12 18:38:33 -0300

    feat: incluído método para inserir registros e exibir em tela

--- node/index.js
+++ node/index.js
@@ -2,8 +2,63 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function createMySQLConnection () {
+    const databaseConfig = {
+        host: 'database',
+        user: 'root',
+        password: 'root',
+        database: 'nodedb'
+    };
+    
+    const mysql = require('mysql');
+    const connection = mysql.createPool(databaseConfig);
+    
+    connection.on('error', function(err) {
+        throw `Falha na conexão: ${err}`
+    })
+
+    console.log('Conexão MySQL obtida com sucesso')
+
+    return connection
+}
+
+function insertPeople () {
+    const connection = createMySQLConnection();
+
+    return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO people SET ?', {name: 'Luis Mascarenhas'}, (error, result) => {
+            if (error) {
+                return reject(error)
+            }
+            console.log(`Retorno dentro do resolve: ${result.insertId}`)
+            return resolve(result.insertId)
+        })
+    })
+}
+
+function getPeopleById (id) {
+    const connection = createMySQLConnection();
+
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM people WHERE id = ${id}`, (error, result) => {
+            if (error) {
+                return reject(error)
+            }
+            console.log(`Retorno dentro do resolve do getPeopleById(): ${result}`)
+            return resolve(result[0])
+        })
+    })
+}
+
 app.get('/', (req, res) => {
-    res.send('<h1> Full Cycle Rocks!</h1>')
+
+    insertPeople().then((id) => {
+        getPeopleById(id).then((result) => {
+            const {id, name} = result
+            res.send(`<h1> Full Cycle Rocks!!</h1> <br> <ul><li>${id} | ${name}</li></ul>`)
+            console.log(`id: ${id} nome: ${name}`)
+        });
+    });
 })
 
 app.listen(port, () => {
